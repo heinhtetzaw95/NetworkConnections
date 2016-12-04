bc8b0911568a91a844e468c51b2abe15f0043640
Hein Htet Zaw <thanlone1995@gmail.com>
2016-12-04 03:46:24 -0600

    got it working - need to clean up and format

--- NetworkConnections/NetworkConnections.cpp
+++ NetworkConnections/NetworkConnections.cpp
@@ -35,14 +35,13 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <math.h>
 
 using namespace std;
 
+		//define maximum numbers for nodes and networks
 #define maxNodes 9
-#define maxSets 10
-
-void header(ofstream&);
-void footer(ofstream&);
+#define maxNets 10
 
 		//setup a structure type Node
 struct node {
@@ -55,47 +54,153 @@ struct node {
 struct network {
 
 			//give network node count and actual nodes
-	int nodeCount;
 	node nodes[maxNodes];
+	int nodeCount;
+	int connectionCount = 0;
+	int connection[maxNodes];
+	double shortest = 999999;
+	double distanceMatrix[maxNodes][maxNodes];
 };
+
+		//declare function names to define later
+double getDistance(node, node);
+void header(ofstream&);
+void footer(ofstream&);
 
 int main() {
 
 			//set up input and output files
-	ifstream infile("Data3.txt", ios::in);
+	ifstream infile("testData.txt", ios::in);
 	ofstream outfile("Output.txt", ios::out);
 
-	network networks[maxSets];
+	network networks[maxNets];
 	
 			//print header section before anything
 	header(outfile);
 
 			//setup number of nodes for the first network and network counter
 	int numNodes = 0;
-	int counter = 0;
+	int networkCount = 0;
+	double totalLength = 0;
 
 			//read in the number of nodes for the first network
 	infile >> numNodes;
 	while (numNodes > 1) {
 
 				//give the firstwork the number of nodes
-		networks[counter].nodeCount = numNodes;
-
-		cout << networks[counter].nodeCount << endl;
+		networks[networkCount].nodeCount = numNodes;
 
 				//read in lat and long for each node of the network
-		for (int i = 0; i < networks[counter].nodeCount; i++) {
-			infile >> networks[counter].nodes[i].lat;
-			infile >> networks[counter].nodes[i].lng;
-
-			cout << networks[counter].nodes[i].lat << " | " << networks[counter].nodes[i].lng << endl;
+		for (int i = 0; i < networks[networkCount].nodeCount; i++) {
+			infile >> networks[networkCount].nodes[i].lat;
+			infile >> networks[networkCount].nodes[i].lng;
 		}
 
 				//increment the network counter
-		counter++;
+		networkCount++;
 
 				//get number of nodes for the next network
 		infile >> numNodes;
+	}
+
+			//find the shortest path for each network
+	for (int n= 0; n< networkCount; n++) {
+		int from, to;
+
+				//find the distance matrix and store the shortest connection
+		for (int i = 0; i < networks[n].nodeCount; i++) {
+			for (int j = i + 1; j < networks[n].nodeCount; j++) {
+
+						//calculate the distance matrix
+				networks[n].distanceMatrix[i][j] = getDistance(networks[n].nodes[i], networks[n].nodes[j]);
+				networks[n].distanceMatrix[j][i] = getDistance(networks[n].nodes[i], networks[n].nodes[j]);
+				if (networks[n].shortest > networks[n].distanceMatrix[i][j]) {
+
+							//store the shortest connection distance
+					networks[n].shortest = networks[n].distanceMatrix[i][j];
+					from = i;
+					to = j;
+				}
+				//cout << i << " to " << j << " : " << networks[n].distanceMatrix[i][j] << endl;
+			}
+		}
+
+				//store the nodes that are already connected
+		networks[n].connection[networks[n].connectionCount] = to;
+		networks[n].connectionCount++;
+		networks[n].connection[networks[n].connectionCount] = from;
+		networks[n].connectionCount++;
+
+		totalLength = networks[n].shortest + 16;
+
+		cout << "shortest: " << networks[n].shortest+16 << endl;
+
+		cout << "(" << networks[n].nodes[from].lat << ","
+			<< networks[n].nodes[from].lng << ") connects "
+			<< "(" << networks[n].nodes[to].lat << ","
+			<< networks[n].nodes[to].lng << ")" << endl << endl;
+
+				//look for the rest of the connection path
+		for (int i = 0; i < networks[n].nodeCount - 2; i++) {
+			double shortest = 9999;
+			int nextTo;
+			bool extendBack;
+
+			for (int j = 0; j < networks[n].nodeCount ; j++) {
+
+				if (!(find(begin(networks[n].connection), 
+					end(networks[n].connection), j) != end(networks[n].connection))
+					&& shortest > networks[n].distanceMatrix[from][j]) {
+
+					shortest = networks[n].distanceMatrix[from][j];
+					extendBack = false;
+
+					//cout << "Extend From: " << networks[n].distanceMatrix[from][j] << endl;
+
+					nextTo = j;
+				}
+
+				if (!(find(begin(networks[n].connection), 
+					end(networks[n].connection), j) != end(networks[n].connection))
+					&& shortest > networks[n].distanceMatrix[to][j]) {
+
+					shortest = networks[n].distanceMatrix[to][j];
+					extendBack = true;
+
+					//cout << "Extend To: " << networks[n].distanceMatrix[to][j] << endl;
+
+					nextTo = j;
+				}
+
+			}
+
+			cout << "Next shortest: " << shortest + 16 << endl;
+
+			if (extendBack) {
+				cout << "(" << networks[n].nodes[to].lat << ","
+					<< networks[n].nodes[to].lng << ") connects "
+					<< "(" << networks[n].nodes[nextTo].lat << ","
+					<< networks[n].nodes[nextTo].lng << ")" << " Extend Back" << endl;
+				to = nextTo;
+			}
+			else {
+				cout << "(" << networks[n].nodes[from].lat << ","
+					<< networks[n].nodes[from].lng << ") connects "
+					<< "(" << networks[n].nodes[nextTo].lat << ","
+					<< networks[n].nodes[nextTo].lng << ")" << " Extend Front" << endl;
+				from = nextTo;
+			};
+
+			totalLength += shortest + 16;
+
+			networks[n].connection[networks[n].connectionCount] = nextTo;
+			networks[n].connectionCount++;
+
+			cout << endl;
+		}
+
+		cout << "Total Length: " << totalLength << endl << endl;
+		cout << "================================================" << endl;
 	}
 
 			//print footer after everything
@@ -104,6 +209,17 @@ int main() {
 	cin.get();
 
 	return 0;
+}
+
+//*****************************************************************************************************
+double getDistance(node A, node B) {
+
+			// Receives – two nodes
+			// Task - find the distance between two nodes
+			// Returns - the distance
+
+	return sqrt(pow(A.lat - B.lat, 2) + pow(A.lng - B.lng, 2));
+
 }
 
 //*****************************************************************************************************
